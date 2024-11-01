0791778caee4b91353254f3913b5e208b8e2d417
Pedro Henrique Delasalle Silva Ribeiro <pedrodelasalle12@gmail.com>
2024-11-01 15:30:21 -0300

    Ataulização da formula de calculo
    Mudança da forma de calculo do juros, juntamente com Rodrigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-const vetorJuros = new Array(9.90, 11.90, 13.90, 15.90, 17.90, 19.90, 21.90, 23.90, 25.90, 27.90, 29.90, 19.90);
+const vetorJuros = new Array(9.90, 11.90, 13.90, 15.90, 17.90, 19.90, 21.90, 23.90, 25.90, 27.90, 29.90, 19.99);
 document.getElementById('calculate').addEventListener('click', function calcular(){
     const value = document.getElementById('value').value; 
     const parcela = document.querySelector('select').value;
     const fee = (vetorJuros[parcela-1])/100;
     
-    const total = value/(1 - fee);
+    const total = value + (value * fee);
     const parcelas = total/parcela;
 
     document.getElementById('total').innerHTML = ("R$" + total.toFixed(2));
